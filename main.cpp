cc59c13c4938620bb957002a9ecf844309435c3c
Andrew_Yankee <144984404+CS230350@users.noreply.github.com>
2023-10-25 11:30:57 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,119 @@
+//
+//  main.cpp
+//  week6lesson2
+//
+//  Created by Dmitriy Taldonov on 25/10/23.
+//
+//PROBLEM 1
+#include <iostream>
+using namespace std;
+
+//void fun(){
+//    int num1;
+//    int num2;
+//    cout<<"Enter the first number: "<< endl;
+//    cin>>num1;
+//    cout<<"enter the second number: "<< endl;
+//    cin>>num2;
+//    int sum = num1+num2;
+//    cout<<sum;
+//    }
+//
+//
+//
+
+
+
+//PROBLEM 4
+
+//int main(){
+//    int n;
+//    cout << "Enter an intrger: ";
+//    cin >> n;
+//    if (n % 2 == 0){
+//    cout << n << "is even.";
+//    }else{
+//    cout << n << "is odd.";
+//    }
+//
+//return 0;
+//}
+
+//Problem 5
+
+//int main(){
+//
+//    bool isNotPrime(int number){
+//        for(int i = 2; i <= number/2; i++){
+//            if (number % i == 0){ return true}
+//           }
+//    }
+//    return false;
+//}
+//int number;
+//cin >> number;
+//bool isNumberPrime = isNotprime(number);
+//if(isnumberPrime)
+
+//FFFFFFFFFIIIIIIIIXXXXXXXXXXXX
+
+
+
+//PROBLEM 6
+
+//void factorial(int number){
+//    int sum = 1;
+//    for (int i = 1; i<= number; i++){
+//        sum *= i;
+//    }
+//    cout << "Factorial"<< sum << endl;
+//}
+
+
+//Problem 8
+int main(){
+    
+int x;
+cout << "Enter the first number: ";
+cin >> x;
+int y;
+cout << "Enter the second number: ";
+cin >> y;
+    switch(x,y){
+        case 1: cout<<"addition"<<x+y<<endl;
+            break;
+        case 2: cout<<"subtraction"<<x-y<<endl;
+            break;
+        case 3: cout <<"multiplication"<<x*y<< endl;
+            break;
+        case 4: cout <<"division"<<x/y<<endl;
+            deafult:"something wrong";
+            break;
+            
+//            Fiiiiiiiiixxxxxxx
+    }
+
+    
+    
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int main(){
+////    fun();
+//    factorial(6);
+//    return 0;
+//}
+
+
